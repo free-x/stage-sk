fae5619a5588ece3e779e23a2575101d7cd26922
free-x <oroitburd@gmail.com>
2025-02-01 11:12:37 +0100

    NodeJs 22

--- 00-add-sources/00-run.sh
+++ 00-add-sources/00-run.sh
@@ -1,15 +1,18 @@
 #!/bin/bash -e
 
-if [ ${RELEASE} == "buster" ]
-then
-  install -m 644 files/skextra.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
-  on_chroot apt-key add - < files/nodesource.gpg.key
-else
-  install -m 644 files/skextra-bullseye.list "${ROOTFS_DIR}/etc/apt/sources.list.d/skextra.list"
-  install -m 644 files/nodesource.gpg "${ROOTFS_DIR}/usr/share/keyrings/"
-fi
+#if [ ${RELEASE} == "buster" ]
+#then
+#  install -m 644 files/skextra.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
+#  on_chroot apt-key add - < files/nodesource.gpg.key
+#else
+#  install -m 644 files/skextra-bullseye.list "${ROOTFS_DIR}/etc/apt/sources.list.d/skextra.list"
+#  install -m 644 files/nodesource.gpg "${ROOTFS_DIR}/usr/share/keyrings/"
+#fi
 
-sed -i "s/RELEASE/${RELEASE}/g" "${ROOTFS_DIR}/etc/apt/sources.list.d/skextra.list"
+#sed -i "s/RELEASE/${RELEASE}/g" "${ROOTFS_DIR}/etc/apt/sources.list.d/skextra.list"
+
+install -m 644 files/skextranew.list "${ROOTFS_DIR}/etc/apt/sources.list.d/skextra.list"
+install -m 644 files/nodesource_2024.gpg "${ROOTFS_DIR}/etc/apt/trusted.gpg.d/"
 
 on_chroot << EOF
 apt-get update
