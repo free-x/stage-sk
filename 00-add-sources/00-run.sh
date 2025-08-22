f1035bedb099a89fe394b8cdc2df56cf94afef3a
free-x <oroitburd@gmail.com>
2025-08-22 14:36:18 +0200

    again arch try

--- 00-add-sources/00-run.sh
+++ 00-add-sources/00-run.sh
@@ -2,6 +2,7 @@
 
 install -m 644 files/skextra.sources "${ROOTFS_DIR}/etc/apt/sources.list.d/"
 install -m 644 files/nodesource_2024.gpg "${ROOTFS_DIR}/usr/share/keyrings/"
+sed -i "s/ARCH/$ARCH/g" "${ROOTFS_DIR}/etc/apt/sources.list.d/skextra.sources" 
 
 on_chroot << EOF
 apt-get update
