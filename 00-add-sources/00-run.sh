9a9c523e83bf053ca8759bf77c3551497ffc6681
free-x <oroitburd@gmail.com>
2025-08-22 09:24:45 +0200

    correct arch

--- 00-add-sources/00-run.sh
+++ 00-add-sources/00-run.sh
@@ -3,8 +3,7 @@
 install -m 644 files/skextra.sources "${ROOTFS_DIR}/etc/apt/sources.list.d/"
 install -m 644 files/nodesource_2024.gpg "${ROOTFS_DIR}/usr/share/keyrings/"
 
-sed -i "s/ARCH/$(dpkg --print-architecture)/g" "${ROOTFS_DIR}/etc/apt/sources.list.d/skextra.sources"
-
 on_chroot << EOF
+sed -i "s/ARCH/$(dpkg --print-architecture)/g" /etc/apt/sources.list.d/skextra.sources
 apt-get update
 EOF
