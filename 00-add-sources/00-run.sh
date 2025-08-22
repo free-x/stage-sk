1a7705c4e5486dfc459626c0101844e7331cf230
free-x <oroitburd@gmail.com>
2025-08-22 10:16:52 +0200

    eliminate not working stuff

--- 00-add-sources/00-run.sh
+++ 00-add-sources/00-run.sh
@@ -4,6 +4,5 @@ install -m 644 files/skextra.sources "${ROOTFS_DIR}/etc/apt/sources.list.d/"
 install -m 644 files/nodesource_2024.gpg "${ROOTFS_DIR}/usr/share/keyrings/"
 
 on_chroot << EOF
-sed -i "s/ARCH/$(dpkg --print-architecture)/g" /etc/apt/sources.list.d/skextra.sources
 apt-get update
 EOF
