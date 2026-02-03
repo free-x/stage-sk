71f7a97b57f5c3f0b5938f1e099f558f99131efb
free-x <oroitburd@gmail.com>
2026-02-03 13:33:04 +0100

    change gpg key

--- 00-add-sources/00-run.sh
+++ 00-add-sources/00-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 install -m 644 files/skextra.sources "${ROOTFS_DIR}/etc/apt/sources.list.d/"
-install -m 644 files/nodesource_2024.gpg "${ROOTFS_DIR}/usr/share/keyrings/"
+install -m 644 files/nodesource-repo.gpg "${ROOTFS_DIR}/usr/share/keyrings/"
 sed -i "s/ARCH/${ARCH}/g" "${ROOTFS_DIR}/etc/apt/sources.list.d/skextra.sources" 
 
 on_chroot << EOF
